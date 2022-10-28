5423682846dd0490c040594770dc527cf2f57e15
Rasmus Karschny <karschny@safelog.de>
2022-10-28 14:24:48 +0200

    C-I to grep under cursor in open buffers

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -20,5 +20,5 @@ require("telescope").load_extension "file_browser"
 vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-N>', ":Telescope file_browser<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-I>', "<cmd>lua require('telescope.builtin').live_grep({grep_open_files=true})<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-I>', "<cmd>lua require('telescope.builtin').grep_string({grep_open_files=true})<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-B>', "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
