9696ab2078cfec9e40f36fd0d99a7e3904840a09
Rasmus Karschny <rkarschny@gmail.com>
2022-11-01 09:04:21 +0100

    keybindings and removed grep_string only on open_files

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -20,5 +20,6 @@ require("telescope").load_extension "file_browser"
 vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-N>', ":Telescope file_browser<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-I>', "<cmd>lua require('telescope.builtin').grep_string({grep_open_files=true})<CR>", { noremap = true })
+-- vim.api.nvim_set_keymap('n', '<C-W>', "<cmd>lua require('telescope.builtin').grep_string({grep_open_files=true})<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-W>', "<cmd>lua require('telescope.builtin').grep_string()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-B>', "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
