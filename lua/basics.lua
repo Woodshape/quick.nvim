2677e06fb9970efb66da5fc210c3fde7d707d1a0
Rasmus Karschny <rkarschny@gmail.com>
2022-11-01 08:45:05 +0100

    Removed ii keybind

--- lua/basics.lua
+++ lua/basics.lua
@@ -26,7 +26,6 @@ local options = { noremap = true }
 
 vim.api.nvim_set_keymap("n", "<leader>s", ":source %<CR>", options)
 
-vim.api.nvim_set_keymap("i", "ii", "<Esc>", options)
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", options)
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", options)
 vim.api.nvim_set_keymap("i", "kj", "<Esc>", options)
