f760881735d6307fd7f93819014793d07575c1bb
Rasmus Karschny <karschny@safelog.de>
2022-11-02 09:46:23 +0100

    Rebind and vim_sheet

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -21,5 +21,5 @@ vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').fin
 vim.api.nvim_set_keymap('n', '<C-N>', ":Telescope file_browser<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
 -- vim.api.nvim_set_keymap('n', '<C-W>', "<cmd>lua require('telescope.builtin').grep_string({grep_open_files=true})<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-W>', "<cmd>lua require('telescope.builtin').grep_string()<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-Q>', "<cmd>lua require('telescope.builtin').grep_string()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-B>', "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
